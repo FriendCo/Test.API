5b1c93745149540e302bda7c6f8224c7227fc764
Fri2ends <63165093+Fri2ends@users.noreply.github.com>
2020-11-20 17:32:22 +0330

    rebuild

--- TestApi/Test.API.Mobile/Test.API.Mobile.Android/obj/Debug/90/android/environment.x86.s
+++ TestApi/Test.API.Mobile/Test.API.Mobile.Android/obj/Debug/90/android/environment.x86.s
@@ -71,7 +71,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",@progbits,1
 	.type	.L.env.str.8, @object
 .L.env.str.8:
-	.asciz	"d824a0dd-82e5-4309-9219-0c9cdef874df"
+	.asciz	"4e395150-1d6b-4cb7-a3cc-4752e1bb3dfa"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",@progbits,1
 	.type	.L.env.str.9, @object
